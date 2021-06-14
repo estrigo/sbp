deca3a0787a38fe038822481d2c09dbedd50c03a
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2021-06-14 09:56:38 +0600

    Added plugin rest controller

---
+++ crm-root/src/main/java/crm/controller/PluginController.java
@@ -0,0 +1,33 @@
+package crm.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import crm.plugin.PluginWrapperSerializer;
+import org.pf4j.PluginManager;
+import org.pf4j.PluginWrapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/plugins")
+public class PluginController {
+
+    @Autowired
+    private PluginManager pluginManager;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @RequestMapping(value = "/all")
+    public ArrayNode allPlugins() {
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(PluginWrapper.class, new PluginWrapperSerializer());
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(simpleModule);
+        return objectMapper.convertValue(pluginManager.getPlugins(), ArrayNode.class);
+    }
+
+}
