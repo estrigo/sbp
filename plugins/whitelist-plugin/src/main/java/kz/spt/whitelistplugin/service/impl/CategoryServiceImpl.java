0d04770704b3cf2b3927c7aeab38e5162cd78291
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2021-09-02 15:28:15 +0600

    Corrected Whitelist plugin start up

--- plugins/whitelist-plugin/src/main/java/kz/spt/whitelistplugin/service/impl/CategoryServiceImpl.java
+++ plugins/whitelist-plugin/src/main/java/kz/spt/whitelistplugin/service/impl/CategoryServiceImpl.java
@@ -3,7 +3,9 @@ package kz.spt.whitelistplugin.service.impl;
 import kz.spt.whitelistplugin.model.Category;
 import kz.spt.whitelistplugin.repository.CategoryRepository;
 import kz.spt.whitelistplugin.service.CategoryService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     private CategoryRepository categoryRepository;
