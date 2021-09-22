717fefb96d2b18b6a30f26c467101bc68f31c233
javadev <mgaiidarov@gmail.com>
2021-09-22 15:56:09 +0600

    Customer db connected, save working

--- spt-root/src/main/java/kz/spt/app/controller/CustomerController.java
+++ spt-root/src/main/java/kz/spt/app/controller/CustomerController.java
@@ -2,12 +2,16 @@ package kz.spt.app.controller;
 
 
 import kz.spt.lib.model.Customer;
+import kz.spt.lib.model.Parking;
 import kz.spt.lib.service.CustomerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -30,4 +34,19 @@ public class CustomerController {
         return "customers/list";
     }
 
+   @GetMapping("/add")
+    public String showFormAddCustomers(Model model) {
+        model.addAttribute("customer", new Customer());
+        return "customers/add";
+    }
+
+    @PostMapping("/add")
+    public String processRequestAddParking(Model model, @Valid Customer customer, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "customers/add";
+        } else {
+            customerService.saveCustomer(customer);
+            return "redirect:/customers/list";
+        }
+    }
 }
