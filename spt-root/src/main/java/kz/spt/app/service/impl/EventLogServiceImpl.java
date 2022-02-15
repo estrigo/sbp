2efbeab83226b1ef8cb77242d1d0d4bf4960575c
angsabay.beybit <aka.beka94@mail.ru>
2022-02-15 15:55:30 +0600

    Send event to tgbot error

--- spt-root/src/main/java/kz/spt/app/service/impl/EventLogServiceImpl.java
+++ spt-root/src/main/java/kz/spt/app/service/impl/EventLogServiceImpl.java
@@ -24,8 +24,6 @@ import org.pf4j.PluginWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -33,8 +31,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -42,7 +38,6 @@ import org.thymeleaf.util.StringUtils;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -117,26 +112,25 @@ public class EventLogServiceImpl implements EventLogService {
 
     public void sendEventBot(ObjectNode node) {
 
-        String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+        try{
+            String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
 
-        URI uri = URI.create(baseUrl);
-        try {
+            URI uri = URI.create(baseUrl);
             uri = new URI(uri.getScheme(), uri.getHost(), uri.getPath(), uri.getFragment());
-        } catch (URISyntaxException e) {
+            String url = uri.toString() + ":8081/event";
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<ObjectNode> requestEntity = new HttpEntity<>(node, headers);
+
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
+        }
+        catch (URISyntaxException e){
             e.printStackTrace();
         }
-        String url = uri.toString() + ":8081/event";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<ObjectNode> requestEntity = new HttpEntity<>(node, headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-        try {
-            ResponseEntity<String> response = restTemplate.postForEntity(url, requestEntity, String.class);
-        } catch (Exception e ) {
-            if (e instanceof org.springframework.web.client.ResourceAccessException) {
-            }
+        catch (Exception e){
+            //e.printStackTrace();
         }
 
     }
