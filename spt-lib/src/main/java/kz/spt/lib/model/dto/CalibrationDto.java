ed8216239967dde77df77a699ca6c2a0dbfd95d8
angsabay.beybit <aka.beka94@mail.ru>
2022-03-03 15:55:28 +0600

    plugin dependencies

--- spt-lib/src/main/java/kz/spt/lib/model/dto/CalibrationDto.java
+++ spt-lib/src/main/java/kz/spt/lib/model/dto/CalibrationDto.java
@@ -3,9 +3,11 @@ package kz.spt.lib.model.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class CalibrationDto {
     private Long id;
