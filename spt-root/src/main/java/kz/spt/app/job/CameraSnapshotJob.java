8f959d046019790596d295c5f88d22bcf6c63e7d
angsabay.beybit <aka.beka94@mail.ru>
2022-03-04 19:09:47 +0600

    Remove yesterday snapshot

--- spt-root/src/main/java/kz/spt/app/job/CameraSnapshotJob.java
+++ spt-root/src/main/java/kz/spt/app/job/CameraSnapshotJob.java
@@ -7,13 +7,20 @@ import kz.spt.lib.service.ArmService;
 import kz.spt.lib.service.CarImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.task.TaskRejectedException;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -28,6 +35,9 @@ public class CameraSnapshotJob {
     private final CameraService cameraService;
     private final ArmService armService;
     private final CarImageService carImageService;
+
+    @Value("${images.file.path}")
+    private String imagePath;
 
     //@Scheduled(cron = "0 0 * * * *")
     @Scheduled(fixedDelay = 5000)
@@ -60,6 +70,35 @@ public class CameraSnapshotJob {
                }catch (TaskRejectedException e){
                    snapshotTaskExecutor.initialize();
                }
+            }
+        });
+    }
+
+    @Scheduled(cron = "0 1 1 * * ?")
+    public void clean(){
+        cameraService.cameraList().forEach(camera->{
+            if (StringUtils.isEmpty(camera.getSnapshotUrl())) return;
+
+            SimpleDateFormat format = new SimpleDateFormat("HHmmss");
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(new Date());
+            calendar.add(Calendar.DATE,-1);
+
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH) + 1;
+            int day = calendar.get(Calendar.DATE);
+
+            String cameraPath = imagePath +"/"+camera.getIp();
+            String path = cameraPath + "/" + year + "/" + month + "/" + day + "/";
+
+            File cameraDir = new File(path);
+            if(cameraDir.exists()){
+                try {
+                    FileUtils.deleteDirectory(cameraDir);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
