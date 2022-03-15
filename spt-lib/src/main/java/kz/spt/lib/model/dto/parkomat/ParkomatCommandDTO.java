9481475843bb4efc70f11318f6304c96d5573de3
Каиржан <kairzhan79@gmail.com>
2022-03-15 17:19:14 +0600

    fix webkassa card pay

--- spt-lib/src/main/java/kz/spt/lib/model/dto/parkomat/ParkomatCommandDTO.java
+++ spt-lib/src/main/java/kz/spt/lib/model/dto/parkomat/ParkomatCommandDTO.java
@@ -7,20 +7,38 @@ import lombok.Getter;
 import java.math.BigDecimal;
 import java.util.Locale;
 
+/**
+ * <h1>Команда поступающая от паркомата</h1>
+ *
+ * @author Kairzhan
+ */
 @Data
 public class ParkomatCommandDTO {
-
+    //Plate number
     @Getter(AccessLevel.NONE)
     String account;
-
+    //Transaction id from parkomat
     String txn_id;
+    //Command name
     String command;
-    BigDecimal sum;
-    String type;
-    String parkomat;
-    BigDecimal change;
-    String paymentType;
 
+    //Payment amount
+    BigDecimal sum;
+
+    //Payment type like cash or ikkm
+    String type;
+
+    // Parkomat provider name
+    String parkomat;
+
+    //Amount of change payed by cash
+    BigDecimal change;
+
+
+    /**
+     * Platenumber in upper case
+     * @return plate number in upper case
+     */
     public String getAccount() {
         return account!=null ? account.toUpperCase(Locale.ROOT) : "";
     }
