740933c8461fe52b78d28468a91bc205f91b2aed
azamatordabekov <ordaz.az@mail.ru>
2022-05-12 11:03:12 +0600

    new payment result if the same transaction

--- plugins/billing-plugin/src/main/java/kz/spt/billingplugin/extension/CommandExecutor.java
+++ plugins/billing-plugin/src/main/java/kz/spt/billingplugin/extension/CommandExecutor.java
@@ -63,14 +63,9 @@ public class CommandExecutor implements PluginRegister {
                 PaymentProvider provider = getPaymentProviderService().getProviderByClientId(command.get("clientId").textValue());
                 List<Payment> oldPayments = getPaymentService().findByTransactionAndProvider(command.get("transaction").textValue(), provider);
 
-                for (Payment p : oldPayments) {
-                    log.info(p.getTransaction());
-                }
-                log.info("PAID for car");
                 if (oldPayments.size() > 0) {
-                    node.put("paymentError", "txn_id уже зарегистрирован");
-                    log.info("it is here");
-                    node.put("paymentErrorCode", 1);
+                    node.put("paymentError", "платеж с таким ID транзакции уже оплачен");
+                    node.put("paymentErrorCode", 10);
                     return node;
                 }
 
