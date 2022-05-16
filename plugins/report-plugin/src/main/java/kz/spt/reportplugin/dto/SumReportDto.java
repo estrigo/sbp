79c50f3ee92cd309874d483292fa617caef81bd4
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2022-05-16 20:51:25 +0600

    Sum report: flexible for all objects

--- plugins/report-plugin/src/main/java/kz/spt/reportplugin/dto/SumReportDto.java
+++ plugins/report-plugin/src/main/java/kz/spt/reportplugin/dto/SumReportDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.*;
 
 @Data
 @Builder
@@ -13,13 +14,6 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class SumReportDto {
 
-    private String dateTime;
-    private int count;
-    private int freeCount;
-    private int paymentsCount;
-    private int whitelistsCount;
-    private int abonementsCount;
-    private BigDecimal kaspiSum;
-    private BigDecimal yurtaSum;
-    private BigDecimal totalSum;
+    Map<String,String> fields = new HashMap<>();
+    Map<String, Object> results = new HashMap<>();
 }
--- plugins/report-plugin/src/main/java/kz/spt/reportplugin/service/impl/SumReportServiceImpl.java
+++ plugins/report-plugin/src/main/java/kz/spt/reportplugin/service/impl/SumReportServiceImpl.java
@@ -20,6 +20,7 @@ import kz.spt.reportplugin.service.RootServicesGetterService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.jvnet.hk2.annotations.Service;
+import org.springframework.context.i18n.LocaleContextHolder;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -27,10 +28,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -55,43 +53,114 @@ public class SumReportServiceImpl implements ReportService<SumReportDto> {
 
     public List<SumReportDto> countSum(FilterSumReportDto filterSumReportDto){
 
-//        List<String> providers = entityManager.createNativeQuery("select * fr");
+        List<Object[]> providers = entityManager.createNativeQuery("select pp.client_id, pp.name, pp.cashless_payment from payment_provider pp where name not like ('%test%')").getResultList();
 
-        String query = "select DATE_FORMAT(date_add(cs.out_timestamp, INTERVAL 6 hour), '%Y.%m.%d') as datetime, " +
+        Boolean hasCashPayment = false;
+        for(Object[] provider: providers){
+            Boolean cashless = (Boolean) provider[2];
+            if(cashless != null && !cashless){
+                hasCashPayment = true;
+            }
+        }
+
+        ArrayList<String> fields = new ArrayList<>(10);
+
+        String headerQuery = "select DATE_FORMAT(date_add(cs.out_timestamp, INTERVAL 6 hour), '%Y.%m.%d') as datetime, " +
                 "       count(cs.id) as count, " +
                 "       count(cs.amount) as paymentsCount, " +
                 "       count(cs.whitelist_json) as whitelistsCount, " +
-                "       count(cs.abonoment_json) as abonementCount, " +
-                "       sum(payments.kaspiSumma) as kaspiSumma, " +
-                "       sum(payments.yurtaSumma) as yurtaSumma, " +
-                "       sum(payments.totalSumma) as totalSum " +
+                "       count(cs.abonoment_json) as abonementCount";
+        fields.add("dateTime");
+        fields.add("records");
+        fields.add("paymentRecords");
+        fields.add("whitelistRecords");
+        fields.add("abonementRecords");
+
+        if(hasCashPayment){
+            headerQuery = headerQuery +
+                    "        ,sum(payments.cardsSumma) as cardsSumma " +
+                    "        ,sum(payments.cashSumma) as cashSumma ";
+            fields.add("bankCardSum");
+            fields.add("cashSum");
+        }
+        for(Object[] provider: providers){
+            headerQuery = headerQuery +
+                    "       ,sum(payments." + provider[0] + "Summa) as " + provider[0] + "Summa ";
+            fields.add((String) provider[1]);
+        }
+
+        headerQuery = headerQuery +
+                "       ,sum(payments.totalSumma) as totalSum ";
+        fields.add("totalSum");
+
+        String bodyQuery =
                 "from car_state cs " +
                 "    left outer join ( " +
                 "        select p.car_state_id as car_state_id, " +
-                "        sum(p.amount) as totalSumma, " +
-                "        sum(if(pp.client_id = 'kaspi', p.amount, 0)) as kaspiSumma, " +
-                "        sum(if(pp.client_id = 'yurta', p.amount, 0)) as yurtaSumma " +
-                "        from payments p " +
-                "            inner join payment_provider pp on p.provider_id = pp.id " +
-                "        group by p.car_state_id " +
-                "    ) as payments on payments.car_state_id = cs.id " +
-                "where cs.out_timestamp is not null " +
-                "and cs.out_timestamp between :dateFrom and :dateTo " +
-                "group by DATE_FORMAT(date_add(cs.out_timestamp, INTERVAL 6 hour), '%Y.%m.%d') " +
-                "order by DATE_FORMAT(date_add(cs.out_timestamp, INTERVAL 6 hour), '%Y.%m.%d') desc";
+                "        sum(p.amount) as totalSumma ";
 
-        List<Object[]> objects =  entityManager.createNativeQuery(query)
+        if(hasCashPayment){
+            bodyQuery = bodyQuery +
+                "        ,sum(if(pp.cashless_payment = false and p.ikkm = true, p.amount, 0)) as cardsSumma " +
+                "        ,sum(if(pp.cashless_payment = false and (p.ikkm is null or p.ikkm = false), p.amount, 0)) as cashSumma ";
+
+        }
+        for(Object[] provider: providers){
+            bodyQuery = bodyQuery +
+                    "        ,sum(if(pp.provider = '" + provider[0] + "', p.amount, 0)) as " + provider[0] + "Summa ";
+        }
+
+        bodyQuery = bodyQuery +
+        " " +
+        "       from payments p " +
+        "            inner join payment_provider pp on p.provider_id = pp.id " +
+        "        group by p.car_state_id " +
+        "    ) as payments on payments.car_state_id = cs.id " +
+        "where cs.out_timestamp is not null " +
+        "and cs.out_timestamp between :dateFrom and :dateTo " +
+        "group by DATE_FORMAT(date_add(cs.out_timestamp, INTERVAL 6 hour), '%Y.%m.%d') " +
+        "order by DATE_FORMAT(date_add(cs.out_timestamp, INTERVAL 6 hour), '%Y.%m.%d') desc";
+
+        List<Object[]> objects = entityManager.createNativeQuery(headerQuery + bodyQuery)
                 .setParameter("dateFrom", filterSumReportDto.getDateFrom())
                 .setParameter("dateTo", filterSumReportDto.getDateTo())
                 .getResultList();
 
         SimpleDateFormat checkFormat = new SimpleDateFormat("yyyy.MM.dd");
-        SimpleDateFormat correctFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+        SimpleDateFormat correctFormat = new SimpleDateFormat("dd.MM.yy HH:mm");
 
         List<SumReportDto> results = new ArrayList<>(objects.size());
 
+        Locale locale = LocaleContextHolder.getLocale();
+        String language = "en";
+        if (locale.toString().equals("ru")) {
+            language = "ru";
+        }
+        ResourceBundle bundle = ResourceBundle.getBundle("report-plugin", Locale.forLanguageTag(language));
+
+        Map<String, String> fieldsMap = new HashMap<>(10);
+        fieldsMap.put("dateTime", bundle.getString("report.dateTime"));
+        fieldsMap.put("records", bundle.getString("report.records"));
+        fieldsMap.put("paymentRecords", bundle.getString("report.paymentRecords"));
+        fieldsMap.put("whitelistRecords", bundle.getString("report.whitelistRecords"));
+        fieldsMap.put("abonementRecords", bundle.getString("report.abonementRecords"));
+        if(hasCashPayment){
+            fieldsMap.put("bankCardSum", bundle.getString("report.bankCardSum"));
+            fieldsMap.put("cashSum", bundle.getString("report.cashSum"));
+        }
+        for(Object[] provider: providers){
+            fieldsMap.put((String) provider[0], (String) provider[1]);
+        }
+        fieldsMap.put("totalSum", bundle.getString("report.totalSum"));
+        SumReportDto firstDto = new SumReportDto();
+        firstDto.setFields(fieldsMap);
+        results.add(firstDto);
+
         for(Object[] object: objects){
-            String dateTime = (String) object[0];
+            int it = 0;
+            SumReportDto sumReportDto = new SumReportDto();
+
+            String dateTime = (String) object[it++];
             StringBuilder dateTimeString = new StringBuilder("");
             if(dateTime.equals(checkFormat.format(filterSumReportDto.getDateFrom()))){
                 dateTimeString.append(correctFormat.format(filterSumReportDto.getDateFrom()));
@@ -114,20 +183,24 @@ public class SumReportServiceImpl implements ReportService<SumReportDto> {
                 dateTimeString.append(dateTime, 2, 4);
                 dateTimeString.append(" 23:59");
             }
-
-            SumReportDto dto = SumReportDto.builder()
-                    .dateTime(dateTimeString.toString())
-                    .count(((BigInteger) object[1]).intValue())
-                    .paymentsCount(((BigInteger) object[2]).intValue())
-                    .whitelistsCount(((BigInteger) object[3]).intValue())
-                    .abonementsCount(((BigInteger) object[4]).intValue())
-                    .kaspiSum(((BigDecimal) object[5]))
-                    .yurtaSum(((BigDecimal) object[6]))
-                    .totalSum(((BigDecimal) object[7]))
-                    //.freeCount(((BigInteger) object[8]).intValue())
-                    .build();
-            results.add(dto);
+            Map<String, Object> values = new HashMap<>(10);
+            values.put("dateTime", dateTimeString);
+            values.put("records", object[it++]);
+            values.put("paymentRecords", object[it++]);
+            values.put("whitelistRecords", object[it++]);
+            values.put("abonementRecords", object[it++]);
+            if(hasCashPayment){
+                values.put("bankCardSum", object[it++]);
+                values.put("cashSum", object[it++]);
+            }
+            for(Object[] provider: providers){
+                values.put((String) provider[0], object[it++]);
+            }
+            values.put("totalSum", object[it++]);
+            sumReportDto.setResults(values);
+            results.add(sumReportDto);
         }
+
         return results;
     }
 
