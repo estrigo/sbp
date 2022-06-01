7ef325ed0e36ec8e2acae4366b53acf80628c1e6
angsabay.beybit <aka.beka94@mail.ru>
2022-06-01 15:04:21 +0600

    Carmen event stream optimization

--- spt-lib/src/main/java/kz/spt/lib/model/dto/carmen/CarmenImage.java
+++ spt-lib/src/main/java/kz/spt/lib/model/dto/carmen/CarmenImage.java
@@ -12,5 +12,5 @@ import java.util.Map;
 @AllArgsConstructor
 public class CarmenImage {
     public Map<String, String> header;
-    public List<Byte> content;
+    public byte[] content;
 }
--- spt-root/src/main/java/kz/spt/app/job/CarmenEventStream.java
+++ spt-root/src/main/java/kz/spt/app/job/CarmenEventStream.java
@@ -3,7 +3,6 @@ package kz.spt.app.job;
 import kz.spt.app.model.dto.CameraStatusDto;
 import kz.spt.app.model.dto.GateStatusDto;
 import kz.spt.lib.model.dto.carmen.CarmenImage;
-import kz.spt.lib.model.dto.carmen.CarmenThread;
 import kz.spt.lib.service.CarEventService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -30,8 +29,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.*;
-import java.util.stream.Collectors;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Log
 @Component
@@ -43,7 +44,36 @@ public class CarmenEventStream {
     @Value("${carmen.live.enabled}")
     Boolean carmenLiveEnabled;
 
-    private Map<String, CarmenThread> streams = new ConcurrentHashMap<>();
+    @Value("${carmen.image.enabled}")
+    Boolean carmenImageEnabled;
+
+    private Map<String, Future> streams = new HashMap<>();
+
+    private Map<String, String> parseHeader(String headerContent) {
+        if (headerContent.length() <= 0) return null;
+
+        Map<String, String> header = new HashMap<>();
+        var lines = headerContent.split("\n", 100);
+        for (String h : lines) {
+            var parts = h.split(":", 2);
+            if (parts.length != 2) continue;
+            header.put(parts[0], parts[1].replaceAll(" ", "").replaceAll("\r", ""));
+        }
+        return header;
+    }
+
+    private Map<String, String> parseSingeHeader(String headerContent) {
+        if (headerContent.length() <= 0) return null;
+
+        var parts = headerContent.split(":", 2);
+        if (parts.length != 2) return null;
+
+        Map<String, String> header = new HashMap<>();
+        String key = parts[0];
+        String value = parts[1].replaceAll(" ", "").replaceAll("\r", "").replaceAll("\n", "");
+        header.put(key, value);
+        return header;
+    }
 
     @SneakyThrows
     @Scheduled(fixedDelay = 2000)
@@ -67,16 +97,12 @@ public class CarmenEventStream {
                     });
                     try {
                         task.get();
-                        streams.put(frontCamera.carmenIp, new CarmenThread(true, task));
+                        streams.put(frontCamera.carmenIp, task);
                     } catch (Exception ex) {
                         ex.printStackTrace();
                     }
                 } else {
-                    if (streams.containsKey(frontCamera.carmenIp)) {
-                        CarmenThread thread = streams.get(frontCamera.carmenIp);
-                        thread.setActive(false);
-                        streams.remove(frontCamera.carmenIp);
-                    }
+                    if (streams.containsKey(frontCamera.carmenIp)) streams.remove(frontCamera.carmenIp);
                 }
             }
 
@@ -94,16 +120,12 @@ public class CarmenEventStream {
                     });
                     try {
                         task.get();
-                        streams.put(frontCamera2.carmenIp, new CarmenThread(true, task));
+                        streams.put(frontCamera2.carmenIp, task);
                     } catch (Exception ex) {
                         ex.printStackTrace();
                     }
                 } else {
-                    if (streams.containsKey(frontCamera2.carmenIp)) {
-                        CarmenThread thread = streams.get(frontCamera2.carmenIp);
-                        thread.setActive(false);
-                        streams.remove(frontCamera2.carmenIp);
-                    }
+                    if (streams.containsKey(frontCamera2.carmenIp)) streams.remove(frontCamera2.carmenIp);
                 }
             }
 
@@ -121,23 +143,19 @@ public class CarmenEventStream {
                     });
                     try {
                         task.get();
-                        streams.put(backCamera.carmenIp, new CarmenThread(true, task));
+                        streams.put(backCamera.carmenIp, task);
                     } catch (Exception ex) {
                         ex.printStackTrace();
                     }
                 } else {
-                    if (streams.containsKey(backCamera.carmenIp)) {
-                        CarmenThread thread = streams.get(backCamera.carmenIp);
-                        thread.setActive(false);
-                        streams.remove(backCamera.carmenIp);
-                    }
+                    if (streams.containsKey(backCamera.carmenIp)) streams.remove(backCamera.carmenIp);
                 }
             }
         }
     }
 
-    private void stream(String ip, String user, String password) {
-        String url = String.format("http://%s/live/events?user=%s&password=%s", ip, user, password);
+    private void stream_old(String ip, String user, String password) {
+       /* String url = String.format("http://%s/live/events?user=%s&password=%s", ip, user, password);
         ConnectionKeepAliveStrategy keepAliveStrategy = (response, context) -> {
             HeaderElementIterator it = new BasicHeaderElementIterator(response.headerIterator(HTTP.CONN_KEEP_ALIVE));
             while (it.hasNext()) {
@@ -175,9 +193,6 @@ public class CarmenEventStream {
                         Map<String, CarmenImage> imageBuffer = new HashMap<>();
 
                         while (b > -1) {
-                            CarmenThread thread = streams.get(ip);
-                            if (!thread.isActive()) throw new InterruptedException();
-
                             if (header == null) {
                                 headerAccumulator += new String(new byte[]{(byte) b});
                                 int indexOfBoundary = headerAccumulator.indexOf(boundary);
@@ -248,19 +263,161 @@ public class CarmenEventStream {
             e.printStackTrace();
         }
 
-        streams.remove(ip);
+        streams.remove(ip);*/
     }
 
-    private Map<String, String> parseHeader(String headerContent) {
-        if (headerContent.length() <= 0) return null;
-
-        Map<String, String> header = new HashMap<>();
-        var lines = headerContent.split("\n", 100);
-        for (String h : lines) {
-            var parts = h.split(":", 2);
-            if (parts.length != 2) continue;
-            header.put(parts[0], parts[1].replaceAll(" ", "").replaceAll("\r", ""));
+    private void stream(String ip, String user, String password) {
+        String url;
+        if (carmenImageEnabled) {
+            url = String.format("http://%s/live/events?&user=%s&password=%s", ip, user, password);
+        } else {
+            url = String.format("http://%s/live/events?image=0&user=%s&password=%s", ip, user, password);
         }
-        return header;
+
+        ConnectionKeepAliveStrategy keepAliveStrategy = (response, context) -> {
+            HeaderElementIterator it = new BasicHeaderElementIterator(response.headerIterator(HTTP.CONN_KEEP_ALIVE));
+            while (it.hasNext()) {
+                HeaderElement he = it.nextElement();
+                String param = he.getName();
+                String value = he.getValue();
+                if (value != null && param.equalsIgnoreCase("timeout")) {
+                    return Long.parseLong(value) * 1000;
+                }
+            }
+            return 5 * 1000;
+        };
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
+        try (CloseableHttpClient client = HttpClients.custom()
+                .setKeepAliveStrategy(keepAliveStrategy)
+                .setConnectionManager(connManager)
+                .build()) {
+            HttpGet httpGet = new HttpGet(url);
+            try (CloseableHttpResponse response = client.execute(httpGet)) {
+                String boundary = "--IPCamEventStreamBoundary\r\n";
+                String contentTypeStart = "Content-Type: ";
+                String contentLengthStart = "Content-Length: ";
+                String eventIndexStart = "X-Event-Index: ";
+                String timestampStart = "X-Timestamp: ";
+                String headerEnd = "\r\n";
+
+                final HttpEntity entity = response.getEntity();
+                if (entity != null) {
+                    try (InputStream inputStream = entity.getContent()) {
+                        String headerAccumulator = "";
+                        String singleHeaderAccumulator = "";
+
+                        int indexOfBoundary = -1;
+                        int expectedContentLength = -1;
+
+                        CarmenImage eventPack = null;
+                        Map<String, String> header = new HashMap<>();
+                        Map<String, CarmenImage> imageBuffer = new HashMap<>();
+                        List<Byte> binaryContentAccumulator = new ArrayList<>();
+
+                        byte[] boundaryData = null;
+                        byte[] contentTypeData = null;
+                        byte[] contentLengthData = null;
+                        byte[] eventIndexData = null;
+                        byte[] timestampData = null;
+                        byte[] headerEndData = null;
+
+                        int b = inputStream.read();
+                        while (b > -1) {
+                            if (expectedContentLength > -1) {
+                                byte[] content = new byte[expectedContentLength];
+                                b = inputStream.read(content);
+                                eventPack = CarmenImage.builder()
+                                        .header(new HashMap<>(header))
+                                        .content(content)
+                                        .build();
+                                header.clear();
+                                expectedContentLength = -1;
+                            } else {
+                                if (indexOfBoundary > -1) {
+                                    if (singleHeaderAccumulator.isEmpty() && contentTypeData == null) {
+                                        contentTypeData = new byte[contentTypeStart.length()];
+                                        b = inputStream.read(contentTypeData);
+                                        singleHeaderAccumulator += new String(contentTypeData);
+                                    } else if (singleHeaderAccumulator.isEmpty() && contentLengthData == null) {
+                                        contentLengthData = new byte[contentLengthStart.length()];
+                                        b = inputStream.read(contentLengthData);
+                                        singleHeaderAccumulator += new String(contentLengthData);
+                                    } else if (singleHeaderAccumulator.isEmpty() && eventIndexData == null) {
+                                        eventIndexData = new byte[eventIndexStart.length()];
+                                        b = inputStream.read(eventIndexData);
+                                        singleHeaderAccumulator += new String(eventIndexData);
+                                    } else if (singleHeaderAccumulator.isEmpty() && timestampData == null) {
+                                        timestampData = new byte[timestampStart.length()];
+                                        b = inputStream.read(timestampData);
+                                        singleHeaderAccumulator += new String(timestampData);
+                                    } else if (singleHeaderAccumulator.isEmpty() && headerEndData == null) {
+                                        headerEndData = new byte[headerEnd.length()];
+                                        b = inputStream.read(headerEndData);
+                                        singleHeaderAccumulator += new String(headerEndData);
+                                    } else {
+                                        b = inputStream.read();
+                                        String str = new String(new byte[]{(byte) b});
+                                        singleHeaderAccumulator += str;
+                                        if (str.equals("\n")) {
+                                            var h = parseSingeHeader(singleHeaderAccumulator);
+                                            if (h != null) {
+                                                header.putAll(h);
+                                            } else {
+                                                header.put("End", String.valueOf(System.currentTimeMillis()));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    b = inputStream.read();
+                                    String str = new String(new byte[]{(byte) b});
+                                    headerAccumulator += str;
+                                    if (str.equals("\n")) {
+                                        indexOfBoundary = 0;
+                                    }
+                                }
+
+                                if (contentLengthData == null && header.containsKey("Content-Type")) {
+                                    singleHeaderAccumulator = "";
+                                } else if (eventIndexData == null && header.containsKey("Content-Length")) {
+                                    singleHeaderAccumulator = "";
+                                } else if (timestampData == null && header.containsKey("X-Event-Index")) {
+                                    singleHeaderAccumulator = "";
+                                } else if (headerEndData == null && header.containsKey("X-Timestamp")) {
+                                    singleHeaderAccumulator = "";
+                                } else if (header.containsKey("End")) {
+                                    indexOfBoundary = -1;
+                                    headerAccumulator = "";
+                                    singleHeaderAccumulator = "";
+                                    expectedContentLength = Integer.valueOf(header.get("Content-Length"));
+                                }
+                            }
+
+                            if (eventPack != null) {
+                                String timestamp = eventPack.header.get("X-Timestamp");
+                                String contentType = eventPack.header.get("Content-Type");
+                                switch (contentType) {
+                                    case "application/json":
+                                        String eventDescriptor = new String(eventPack.content);
+                                        carEventService.handleLiveStreamEvent(null, eventDescriptor, timestamp);
+                                        log.info(eventDescriptor);
+                                        break;
+                                }
+
+                                eventPack = null;
+                                contentTypeData = null;
+                                contentLengthData = null;
+                                eventIndexData = null;
+                                timestampData = null;
+                                headerEndData = null;
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        streams.remove(ip);
     }
 }
