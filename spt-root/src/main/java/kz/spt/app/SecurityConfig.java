a541d0286111eb9b0c055ab91e3f8f237d53eef9
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2022-06-02 23:13:46 +0600

    Added Baqorda user to balance add and remove operation

--- spt-root/src/main/java/kz/spt/app/SecurityConfig.java
+++ spt-root/src/main/java/kz/spt/app/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and().authorizeRequests()
-                .antMatchers("/balance/**").hasRole("ADMIN")
+                .antMatchers("/balance/**").hasAnyRole("ADMIN", "BAQORDA")
                 .antMatchers("/users/delete/**").hasAnyRole("ADMIN", "OPERATOR", "READ")
                 .antMatchers( "/events/**", "/journal/**", "/arm/**").hasAnyRole("AUDIT", "ADMIN", "MANAGER", "OPERATOR", "OPERATOR_NO_REVENUE_SHARE","RTA","ACCOUNTANT", "READ")
                 .antMatchers( "/customers/**", "/register/**", "/cars/**", "/parking/**", "/customer/**").hasAnyRole("AUDIT", "ADMIN", "MANAGER", "READ")
