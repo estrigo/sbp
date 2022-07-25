fd496b1d5d559a608465c5ba2e792c3ca432fa61
ms-dev <m.stepanov@parqour.xyz>
2022-07-25 10:23:55 +0600

    tune paymentCheckLogs for payment log page https://app.clickup.com/t/32yx0bx

--- spt-root/src/main/java/kz/spt/app/repository/PaymentCheckLogRepository.java
+++ spt-root/src/main/java/kz/spt/app/repository/PaymentCheckLogRepository.java
@@ -1,7 +1,6 @@
 package kz.spt.app.repository;
 
 import kz.spt.lib.model.PaymentCheckLog;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,5 +15,6 @@ public interface PaymentCheckLogRepository extends JpaRepository<PaymentCheckLog
     @Query("from PaymentCheckLog pcl WHERE pcl.carStateId is not null and pcl.paymentCheckType = 'STANDARD' and pcl.plateNumber = ?1 and pcl.created >= ?2 order by pcl.id")
     List<PaymentCheckLog> findLastSuccessCheck(String plateNumber, Date date);
 
+    List<PaymentCheckLog> findPaymentCheckLogByProviderId(Long providerId, Pageable pageable);
     List<PaymentCheckLog> findPaymentCheckLogByProviderId(Long providerId);
 }
