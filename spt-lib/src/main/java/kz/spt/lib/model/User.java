826dd945661c9465a93f5ab0e78c4d7941c631c8
ms-dev <m.stepanov@parqour.xyz>
2022-09-28 11:40:33 +0600

    tune User

--- spt-lib/src/main/java/kz/spt/lib/model/User.java
+++ spt-lib/src/main/java/kz/spt/lib/model/User.java
@@ -40,7 +40,7 @@ public class User {
 
     private int enabled;
 
-    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @ManyToMany
     @JoinTable(name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
