3824dbf9829e70082c91228657cbf7485d5c5c05
Maxat Zhuniskhanov <zhmaksat@gmail.com>
2022-10-11 15:47:10 +0600

    feature  initializenumber on tablo at app startup, changed from constructor to bean

--- spt-root/src/main/java/kz/spt/app/service/impl/TableServiceImpl.java
+++ spt-root/src/main/java/kz/spt/app/service/impl/TableServiceImpl.java
@@ -4,8 +4,10 @@ import kz.spt.app.repository.CarStateRepository;
 import kz.spt.app.repository.GateRepository;
 import kz.spt.lib.model.Gate;
 import kz.spt.lib.service.TabloService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,16 +20,18 @@ import java.util.logging.Level;
 
 @Log
 @Service
+@RequiredArgsConstructor
 @Transactional(noRollbackFor = Exception.class)
 public class TableServiceImpl implements TabloService {
 
     private final GateRepository gateRepository;
     private final CarStateRepository carStateRepository;
 
-    public TableServiceImpl(GateRepository gateRepository, CarStateRepository carStateRepository, @Value("${tablo.connected}") Boolean tabloConnected) {
-        this.gateRepository = gateRepository;
-        this.carStateRepository = carStateRepository;
+    @Value("${tablo.connected}")
+    Boolean tabloConnected;
 
+    @Bean
+    public void updateTablo(){
         if(tabloConnected){
             Iterable<Gate> inGatesWithTabloIp = gateRepository.findByGateTypeAndTabloIpIsNotNull(Gate.GateType.IN);
             for(Gate gate: inGatesWithTabloIp){
