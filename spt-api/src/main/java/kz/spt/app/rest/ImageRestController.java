6834acb7bd0db0f1bfad5566cdb26c2548882dde
spt <Awoljack0@>
2022-10-11 13:54:01 +0600

    fixed all steps

--- spt-api/src/main/java/kz/spt/app/rest/ImageRestController.java
+++ spt-api/src/main/java/kz/spt/app/rest/ImageRestController.java
@@ -4,6 +4,8 @@ import kz.spt.lib.service.CarImageService;
 import lombok.extern.java.Log;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @Log
 @RestController
 @RequestMapping(value = "/files")
@@ -24,9 +26,19 @@ public class ImageRestController {
     @RequestMapping(value = "/pictures/{filename}", method = RequestMethod.GET)
     @ResponseBody
     public byte[] getSnapshot(@PathVariable("filename") String filename, @RequestParam("ver") String ver) throws Exception {
+        return getImage(filename);
+    }
+    @RequestMapping(value = "/pic/v2/{filename}", method = RequestMethod.GET)
+    @ResponseBody
+    public  byte[] giveAnswerV2 (@PathVariable("filename") String filename, @RequestParam("ver") String ver) throws IOException {
+        return getImage(filename);
+    }
+
+    private byte[] getImage (String filename) throws IOException {
         if (filename.substring(1)=="/") {
             return carImageService.getByUrl(filename);
         }
         return carImageService.getByUrl("/" + filename);
     }
 }
+
