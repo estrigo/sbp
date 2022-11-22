02e412bf4e0a8125bb16fcddd2d317efb8003641
Abimoldayeva Tomiris <abimoldayevat@gmail.com>
2022-11-22 18:08:23 +0600

    method overloaded

--- spt-lib/src/main/java/kz/spt/lib/service/LanguagePropertiesService.java
+++ spt-lib/src/main/java/kz/spt/lib/service/LanguagePropertiesService.java
@@ -1,13 +1,19 @@
-package kz.spt.app.service;
+package kz.spt.lib.service;
 
 import java.io.IOException;
 import java.util.Map;
 
 public interface LanguagePropertiesService {
     Map<String, String> getWithDifferentLanguages(String key, Map<String, Object> values);
+
     Map<String, String> getWithDifferentLanguages(String key);
+
     String getMessageFromProperties(String key);
+
     String getMessageFromProperties(String key, String langCode);
+
+    String getMessageFromProperties(String key, String langCode, Map<String, Object> messageValues);
+
     String putValuesToString(String text, Map<String, Object> values);
 
 }
--- spt-root/src/main/java/kz/spt/app/service/impl/LanguagePropertiesServiceImpl.java
+++ spt-root/src/main/java/kz/spt/app/service/impl/LanguagePropertiesServiceImpl.java
@@ -3,7 +3,8 @@ package kz.spt.app.service.impl;
 import com.mitchellbosecke.pebble.PebbleEngine;
 import com.mitchellbosecke.pebble.loader.StringLoader;
 import com.mitchellbosecke.pebble.template.PebbleTemplate;
-import kz.spt.app.service.LanguagePropertiesService;
+import kz.spt.lib.service.Language;
+import kz.spt.lib.service.LanguagePropertiesService;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
@@ -23,17 +24,14 @@ import java.util.stream.Stream;
 @Slf4j
 public class LanguagePropertiesServiceImpl implements LanguagePropertiesService {
 
-    private final String EN = "en";
-    private final String RU = "ru";
-    private final String LOCAL = "local";
     @SneakyThrows
     public Map<String, String> getWithDifferentLanguages(String key, Map<String, Object> values){
         Locale locale = LocaleContextHolder.getLocale();
         Map<String, String> eventsText = new HashMap<>();
 
-        Stream.of(EN, RU, LOCAL)
+        Stream.of(Language.EN, Language.RU, Language.LOCAL)
                 .forEach(lang -> {
-                    String language = lang.equals(LOCAL) ? locale.toString(): lang;
+                    String language = lang.equals(Language.LOCAL) ? locale.toString(): lang;
                     StringBuilder message = new StringBuilder(getMessageFromProperties(key, language));
 
                     if (values.containsKey("additionalMessage") && values.get("additionalMessage") != null){
@@ -51,9 +49,9 @@ public class LanguagePropertiesServiceImpl implements LanguagePropertiesService 
     public Map<String, String> getWithDifferentLanguages(String key) {
         Locale locale = LocaleContextHolder.getLocale();
         Map<String, String> eventsText = new HashMap<>();
-        Stream.of(EN, RU, LOCAL)
+        Stream.of(Language.EN, Language.RU, Language.LOCAL)
                 .forEach(lang -> {
-                    String language = lang.equals(LOCAL) ? locale.toString(): lang;
+                    String language = lang.equals(Language.LOCAL) ? locale.toString(): lang;
                     String message = getMessageFromProperties(key, language);
                     eventsText.put(lang, message);
                 });
@@ -68,7 +66,7 @@ public class LanguagePropertiesServiceImpl implements LanguagePropertiesService 
         try {
             message = bundle.getString(key);
         } catch (Exception e) {
-            ResourceBundle defaultBundle = ResourceBundle.getBundle("messages", Locale.forLanguageTag(EN));
+            ResourceBundle defaultBundle = ResourceBundle.getBundle("messages", Locale.forLanguageTag(Language.EN));
             log.info("key " + key + " for language " + langCode + " not found");
             try {
                 message = defaultBundle.getString(key);
@@ -82,6 +80,30 @@ public class LanguagePropertiesServiceImpl implements LanguagePropertiesService 
         return message;
     }
 
+    @Override
+    public String getMessageFromProperties(String key, String langCode, Map<String, Object> messageValues) {
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.forLanguageTag(langCode));
+        String message = "";
+        String messageWithValues = "";
+        try {
+            message = bundle.getString(key);
+            messageWithValues = putValuesToString(message, messageValues);
+        } catch (Exception e) {
+            ResourceBundle defaultBundle = ResourceBundle.getBundle("messages", Locale.forLanguageTag(Language.EN));
+            log.info("key " + key + " for language " + langCode + " not found");
+            try {
+                message = defaultBundle.getString(key);
+                messageWithValues = putValuesToString(message, messageValues);
+                log.info("Writing to local message in default language en");
+            } catch (Exception ex) {
+                log.info("key " + key + " not found in default message bundle en");
+                messageWithValues = null;
+                log.info("Writing to local message null");
+            }
+        }
+        return messageWithValues;
+    }
+
     public String getMessageFromProperties(String key) {
         Locale locale = LocaleContextHolder.getLocale();
         ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.forLanguageTag(locale.toString()));
@@ -89,13 +111,13 @@ public class LanguagePropertiesServiceImpl implements LanguagePropertiesService 
         try {
             message = bundle.getString(key);
         } catch (Exception e) {
-            ResourceBundle defaultBundle = ResourceBundle.getBundle("messages", Locale.forLanguageTag(EN));
+            ResourceBundle defaultBundle = ResourceBundle.getBundle("messages", Locale.forLanguageTag(Language.EN));
             log.info("key " + key + " for language " + locale.toString() + " not found");
             try {
                 message = defaultBundle.getString(key);
                 log.info("Writing to local message in default language en");
             } catch (Exception ex) {
-                ResourceBundle ruBundle = ResourceBundle.getBundle("messages", Locale.forLanguageTag(RU));
+                ResourceBundle ruBundle = ResourceBundle.getBundle("messages", Locale.forLanguageTag(Language.RU));
                 log.info("key " + key + " for language " + locale.toString() + " not found");
                 try {
                     message = ruBundle.getString(key);
