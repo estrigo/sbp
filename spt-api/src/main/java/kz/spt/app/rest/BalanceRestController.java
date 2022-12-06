2626b295d8e7246a582ed9c455672c58c65a2391
admin <pernebek.askhat@gmail.com>
2022-12-06 17:54:25 +0600

    fix last commit

--- spt-api/src/main/java/kz/spt/app/rest/BalanceRestController.java
+++ spt-api/src/main/java/kz/spt/app/rest/BalanceRestController.java
@@ -1,12 +1,9 @@
 package kz.spt.app.rest;
 
-import kz.spt.lib.model.dto.payment.CommandDto;
-import kz.spt.lib.service.PaymentService;
 import kz.spt.lib.service.PluginService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.math.BigDecimal;
 
 @RestController
@@ -15,17 +12,8 @@ public class BalanceRestController {
 
     private PluginService pluginService;
 
-    private PaymentService paymentService;
-
-    public BalanceRestController(PluginService pluginService, PaymentService paymentService){
+    public BalanceRestController(PluginService pluginService){
         this.pluginService = pluginService;
-        this.paymentService = paymentService;
-    }
-
-    @RequestMapping(value = "/billing", method = RequestMethod.POST, consumes = "application/json;charset=UTF-8", produces = "application/json;charset=UTF-8")
-    @ResponseBody
-    public Object billingInteraction(@Valid @RequestBody CommandDto commandDto) throws Exception{
-        return paymentService.billingInteractions(commandDto);
     }
 
     @GetMapping("/check/{platenumber}")
