eedb5f70c72e83f0bbdd479171979a9f1d53c996
Abimoldayeva Tomiris <abimoldayevat@gmail.com>
2022-12-07 10:40:51 +0600

    added language service

--- plugins/carmodel-plugin/src/main/java/kz/spt/carmodelplugin/service/RootServicesGetterService.java
+++ plugins/carmodel-plugin/src/main/java/kz/spt/carmodelplugin/service/RootServicesGetterService.java
@@ -2,11 +2,14 @@ package kz.spt.carmodelplugin.service;
 
 import kz.spt.lib.service.CarsService;
 import kz.spt.lib.service.EventLogService;
+import kz.spt.lib.service.LanguagePropertiesService;
 
 public interface RootServicesGetterService {
 
     CarsService getCarsService();
 
     EventLogService getEventLogService();
+
+    LanguagePropertiesService getLanguagesService();
 
 }
